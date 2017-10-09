4d8ecdcee93f940d52885d82f635df3b96167d89
Denis Campos <campos_bra@hotmail.com>
2017-10-09 20:46:35 -0300

    Ajust models

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,8 +1,8 @@
 class Link < ActiveRecord::Base
   validates_presence_of :link
 
-  belongs_to :company
-
-  has_many :hashtags, through: :link_hashtags
   has_many :link_hashtags
+  has_many :hashtags, through: :link_hashtags
+
+  belongs_to :company
 end
--- app/models/link_hashtag.rb
+++ app/models/link_hashtag.rb
@@ -1,6 +1,6 @@
 class LinkHashtag < ActiveRecord::Base
   validates_presence_of :link_id, :hashtag_id
-  
-  belongs_to :links
-  belongs_to :hashtags
+
+  belongs_to :link
+  belongs_to :hashtag
 end
